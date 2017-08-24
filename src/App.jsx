c8ccbb5debab54aba12b6e4a06cc04b613e3d283
Maria Isabel Bances <marisaban7@gmail.com>
2017-08-23 21:04:48 -0700

    initalized state in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,28 @@ import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+//set the initial state 
+//React will call the functions in the following order when it mounts
+// the component
+// constructor
+// componentWillMount (not used in this component)
+// componentDidMount
+	constructor(props){
+		super(props);
+		this.state = {
+			currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+			messages: [
+			{
+			  username: "Bob",
+			  content: "Has anyone seen my marbles?",
+			},
+			{
+			  username: "Anonymous",
+			  content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+			}
+			]
+		}
+	}
   render() {
     return (
       <div>
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ const ChatBar = function (props){
     console.log('Rendering <ChatBar />');
     return (
         <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" defaultValue={props.currentUser} placeholder="Your Name (Optional)" />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
